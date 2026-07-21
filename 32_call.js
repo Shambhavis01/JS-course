3b675b2bd476524c2ac88d1cfb64d90d44b2d7f6
Shambhavis01 <ssausep1111@gmail.com>
2026-07-21 18:05:36 +0530

    add call

---
+++ 32_call.js
@@ -0,0 +1,13 @@
+function SetUsername(username){
+    //complex DB calls 
+    this.username=username
+    console.log("called")
+}
+function createUser(username,email,passwords){
+   // SetUsername(username)//not called in output
+    SetUsername.call(this,username)//using call,holds the reference
+    this.email=email
+    this.passwords=passwords
+}
+const kiki= new createUser("kiki","kiki@111.com","999")
+console.log(kiki);
